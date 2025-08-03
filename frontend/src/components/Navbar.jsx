7a49d4c36de63b2f6d41862fb44541d87ce0eb29
Utkarsh-1709 <utkarshmaurya1998@gmail.com>
2025-08-03 11:37:45 +0530

    Navbar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/ChatGPT Image Jul 23, 2025, 01_55_52 PM.png"
+import logo from "../assets/stay_en_casa-nobg.png";
+import "../components/Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav style={{ position: "fixed", 
-                top: 0, 
-                left: 0, 
-                width: "100%",  
-                zIndex: 1000,
-                display: "flex",
-                justifyContent: "normal",
-                alignItems: "center",
-                background: "linear-gradient(90deg, rgb(251,242,222) 70%)",
-                boxShadow: "0 2px 8px rgba(0,0,0,0.07)" }}>
-    <img src={logo} alt="Logo" style={{height: "50px"}} />
-    <div >
-      <Link to="/">Booking</Link> |{" "}
-      <Link to="/my-bookings">My Bookings</Link> |{" "}
-      <Link to="/properties">All Properties</Link>
-    </div>
-    
+    <nav className="navbar">
+      {/* Logo */}
+      <img src={logo} alt="Logo" className="navbar-logo" />
+
+      {/* Links */}
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">Booking</Link>
+        <Link to="/my-bookings" className="nav-link">My Bookings</Link>
+        <Link to="/properties" className="nav-link">All Properties</Link>
+      </div>
     </nav>
   );
 };
